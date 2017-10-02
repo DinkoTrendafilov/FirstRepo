82bb1262f9ae168741abe5226237176e677495be
dinko_dt <dinko_dt@abv.bg>
2017-10-02 12:56:13 +0300

    Added

---
+++ C#Basic Syntax - Exercises/04. Beverage Labels/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.Beverage_Labels
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string foodProduct = Console.ReadLine();
+            double volume = double.Parse(Console.ReadLine());
+            double energyContent = double.Parse(Console.ReadLine());
+            double sugarContent = double.Parse(Console.ReadLine());
+
+            double tottalEnergy = (energyContent / 100) * volume;
+            double tottalSugar = (sugarContent / 100) * volume;
+
+
+            Console.WriteLine($"{volume}ml {foodProduct}:");
+            Console.WriteLine($"{tottalEnergy}kcal, {tottalSugar}g sugars");
+
+        }
+    }
+}
